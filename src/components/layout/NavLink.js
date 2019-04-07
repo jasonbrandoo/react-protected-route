ca755fb0ba1d4a915ad7a6172b6fdaff4c2da3e7
Jason Brando <jasonbrandoo@gmail.com>
2019-04-07 15:32:14 +0700

    update

--- src/components/layout/NavLink.js
+++ src/components/layout/NavLink.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Link as RouterLink } from 'react-router-dom';
 import { Button, withStyles } from '@material-ui/core';
 
-const style = theme => ({
+const style = () => ({
   left: {
     marginLeft: 'auto',
   },
